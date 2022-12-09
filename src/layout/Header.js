11c893e6b362d51ebe0c575ea6d41981eb8310bb
Tanzilahamed <ahmed15-4325@diu.edu.bd>
2022-12-09 12:23:09 -0800

    message

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -19,7 +19,7 @@ const Header = ({ logo }) => {
                 <a href="#about">About</a>
               </li>
               <li>
-                <a href="#portfolio">Portfolio</a>
+                <a href="#portfolio">project</a>
               </li>
               <li>
                 <a href="#service">Service</a>
